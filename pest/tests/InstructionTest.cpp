ecf876490c87a03c158c3467bdff9096e8e658b6
= <=>
2014-01-17 14:26:21 +0100

    began working on tests and source files

--- pest/tests/InstructionTest.cpp
+++ pest/tests/InstructionTest.cpp
@@ -1,6 +1,17 @@
-#include <TraceLine.hpp>
+#include <Instruction.hpp>
 #define BOOST_TEST_MODULE InstrTypeTest
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE ( InstrTypeTest )
+{
+    std::string input_1("mov fp,#0");
+    Instruction instr_1(input_1, Instruction::InstrType::IntAlu);
 
+    BOOST_CHECK_EQUAL(std::strcmp( instr_1.getInstr(), "mov"), 0);
+    BOOST_CHECK_EQUAL(std::strcmp( instr_1.getOp1(), "fp"), 0);
+    BOOST_CHECK_EQUAL(std::strcmp( instr_1.getOp2(), "#0"), 0);
+    BOOST_CHECK_EQUAL(std::strcmp( instr_1.getOp3(), NULL), 0);
+    BOOST_CHECK_EQUAL(instr_1.getNumOp(), 2);
+    BOOST_CHECK_EQUAL(instr_1.getExecType(), Instruction::InstrType::IntAlu);
+
+}
--- pest/tests/TraceLineTest.cpp
+++ pest/tests/TraceLineTest.cpp
@@ -6,15 +6,10 @@
 
 BOOST_AUTO_TEST_CASE ( TraceLine )
 {
-    string input_1 = "      0: system.cpu T0 : 0x89cc    :   mov   fp, #0             : IntAlu :  D=0x0000000000000000";
+    std::string input_1 = "      0: system.cpu T0 : 0x89cc    :   mov   fp, #0             : IntAlu :  D=0x0000000000000000";
     TraceLine tl_1(input1);
     BOOST_CHECK_EQUAL(tl_1.getTick(), 0);
     BOOST_CHECK_EQUAL(tl_1.getCPU(), 0);
     BOOST_CHECK_EQUAL(tl_1.getPC(), 0x89cc);
-    BOOST_CHECK_EQUAL( std::strcmp( tl_1.getInstr(), "mov"), 0);
-    BOOST_CHECK_EQUAL( std::strcmp( tl_1.getOp1(), "fp"), 0);
-    BOOST_CHECK_EQUAL( std::strcmp( tl_1.getOp2(), "#0"), 0);
-    BOOST_CHECK_EQUAL( std::strcmp( tl_1.getOp3(), NULL), 0);
-    BOOST_CHECK_EQUAL(tl_1.getNumOp(), 2);
-    BOOST_CHECK_EQUAL( tl_1.getExecType(), InstrType::IntAlu );
+    BOOST_CHECK_EQUAL(t1_1.getInstr(), Instruction("mov fp,#0", Instruction::IntAlu))
 }
