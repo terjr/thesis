4dddddd3ef3323858a3cfeae553499001aed7bb6
Terje Runde <terje.runde@gmail.com>
2014-01-27 15:04:44 +0100

    Use lexcial_cast instead of C-style casting.

--- pest/src/TraceLine.cpp
+++ pest/src/TraceLine.cpp
@@ -1,20 +1,23 @@
 #include <cstdlib>
 #include <boost/regex.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include "TraceLine.hpp"
 
 using namespace std;
+using boost::lexical_cast;
+
+static const boost::regex pattern(" *([[:digit:]]+) *: *([^ :]+) T([[:digit:]]+) *: *([^:]+): *([^:]+): *([^ :]+) *: *(.*)");
 
 TraceLine::TraceLine(const string line)
 {
-    const boost::regex pattern(" *([[:digit:]]+) *: *([^ :]+) T([[:digit:]]+) *: *([^:]+): *([^:]+): *([^ :]+) *: *(.*)");
     boost::smatch result;
 
     if (boost::regex_search(line, result, pattern) && result.size() == 8 )
     {
-        tick = strtol(string(result[1]).c_str(), NULL, 10);
-        cpu =  strtol(string(result[3]).c_str(), NULL, 10);
-        pc = strtol(string(result[4]).c_str(), NULL, 16);
+        tick = lexical_cast<int>(result[1]);
+        cpu = lexical_cast<int>(result[3]);
+        pc = stoul(result[4], NULL, 16);
         instr = Instruction(result[5], Instruction::instrTypeFromString(result[6]));
     }
     else
