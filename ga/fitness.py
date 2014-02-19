dfbd1577aa4c18db1024d9c5749e732303b25146
Stian Hvatum <stian@hvatum.net>
2014-02-19 11:29:48 +0100

    second iteration of fitness function

--- ga/fitness.py
+++ ga/fitness.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-from subprocess import call
+import subprocess
 
 def distance(graph1, graph2):
     diff = 0
@@ -40,13 +40,13 @@ def read_graph(filename):
 
 
 def write_weights_file(weights, filename):
-    f = open(filename)
+    f = open(filename, 'w+')
     f.write("IntAlu "+str(weights[0])+"\n")
-    f.write("IntMult"+str(weights[1])+"\n")
-    f.write("MemRead"+str(weights[2])+"\n")
-    f.write("MemWrite"+str(weights[3])+"\n")
-    f.write("SimdFloat"+str(weights[4])+"\n")
-    f.write("L1I"+str(weights[5])+"\n")
+    f.write("IntMult "+str(weights[1])+"\n")
+    f.write("MemRead "+str(weights[2])+"\n")
+    f.write("MemWrite "+str(weights[3])+"\n")
+    f.write("SimdFloat "+str(weights[4])+"\n")
+    f.write("L1I "+str(weights[5])+"\n")
     f.write("L1D "+str(weights[6])+"\n")
     f.write("L2 "+str(weights[7])+"\n")
     f.write("Phys "+str(weights[8])+"\n")
@@ -54,14 +54,21 @@ def write_weights_file(weights, filename):
 
 
 def eval_individual(weights):
-    name = str(weights)
-    write_weights_file("/tmp/"+name+"_weights.conf")
+    name = ''.join(map(str, weights))
+    write_weights_file(weights, "/tmp/"+name+"_weights.conf")
     return run_tests("/tmp/"+name+".output", "/tmp/"+name+"_weights.conf")
 
 
 def run_tests(output, weightfile):
-    call(["/home/hvatum/Skole/thesis/pest/pest", "/home/hvatum/Skole/thesis/workloads/m5out/misc/pi/trace.out", "-o", output, "-w", weightfile])
+    prog = ["/home/hvatum/Skole/thesis/pest/pest", "/home/hvatum/Skole/thesis/workloads/m5out/pi-pi/trace.out", "-f", "plain", "-o", output, "-w", weightfile]
+    print(" ".join(prog))
+    subprocess.call(prog)
+#    process = subprocess.Popen(prog, shell=True, stdout=subprocess.PIPE)
+#    process.wait()
+#    print(process.returncode)
     return align(read_graph(output), read_graph("/tmp/pi-power"))
 
+print(eval_individual([1,2,3,4,5,6,7,8,9]))
+print(eval_individual([22,30,15,40,30,3,3,30,90]))
 #idx = align(read_graph(sys.argv[1]), read_graph(sys.argv[2]))
 
