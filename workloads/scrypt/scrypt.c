87d9e9f93e1889a9ed79cf18f78c9ad7854ec789
Terje Runde <terje.runde@gmail.com>
2014-02-24 15:48:28 +0100

    Simpler scrypt algorithm.

--- workloads/scrypt/scrypt.c
+++ workloads/scrypt/scrypt.c
@@ -9,16 +9,7 @@ int main(int argc, char **args) {
     uint8_t *outbuf = malloc(sizeof(uint8_t) * BUFLEN);
     int retcode = 0;
 
-    retcode = libscrypt_scrypt((uint8_t*)"passord", strlen("passord"), (uint8_t*)"sahjt", strlen("sahjt"), SCRYPT_N, SCRYPT_r, SCRYPT_p, outbuf, BUFLEN);
-
-    if (retcode != 0) {
-        printf("fail\n");
-        exit(retcode);
-    }
-    printf("hashed value:\n");
-    for (int i = 0; i < BUFLEN; ++i) {
-        putchar(outbuf[i]);
-    }
+    retcode = libscrypt_scrypt((uint8_t*)"passord", strlen("passord"), (uint8_t*)"sahjt", strlen("sahjt"), SCRYPT_N*4, SCRYPT_r, SCRYPT_p, outbuf, BUFLEN);
 
     return retcode;
 }
