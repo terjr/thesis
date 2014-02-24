3f6161b89016ea0333b1050c7514aafc2df7bf53
Stian Hvatum <stian@hvatum.net>
2014-02-24 15:07:19 +0100

    1+lambda ga algo

--- ga/fitness.py
+++ ga/fitness.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import subprocess
 
 def distance(graph1, graph2):
@@ -26,17 +27,22 @@ def align(graph1, graph2):
         if dist < least_diff:
             least_diff = dist
             best = i
-#    print("Best alignment at idx "+str(best)+" with diff of "+str(least_diff))
+    print("Best alignment at idx "+str(best)+" with diff of "+str(least_diff))
     return least_diff
 
 
 def read_graph(filename):
-    lines = []
-    for line in open(filename):
-        number = line[line.find(' '):-1].strip()
-        if number.isdigit():
-            lines.append(int(number))
-    return lines;
+    with open(filename) as f:
+        return [int(number) for number in [line[line.find(' '):-1].strip() for line in f] if number.isdigit()]
+
+
+#def read_graph(filename):
+#    lines = []
+#    for line in open(filename):
+#        number = line[line.find(' '):-1].strip()
+#        if number.isdigit():
+#            lines.append(int(number))
+#    return lines;
 
 
 def write_weights_file(weights, filename):
@@ -55,21 +61,27 @@ def write_weights_file(weights, filename):
 
 def eval_individual(weights):
     name = ''.join(map(str, weights))
-    write_weights_file(weights, "/tmp/"+name+"_weights.conf")
-    fitness = run_tests("/tmp/"+name+".output", "/tmp/"+name+"_weights.conf")
-    print("Fitness = "+str(fitness))
+    write_weights_file(weights, "/home/hvatum/NAS/stian/Skole/pest-tmp/"+name+"_weights.conf")
+    fitness = run_tests("/home/hvatum/NAS/stian/Skole/pest-tmp/"+name+".output", "/home/hvatum/NAS/stian/Skole/pest-tmp/"+name+"_weights.conf")
+    print("Fitness = "+str(fitness)+"\n")
     return (fitness,)
+
+
+def run_test(output, weightfile, test):
+    tf = output + "-" + test
+    hw = "/home/hvatum/NAS/stian/Skole/pest-tmp/" + test + "-power";
+    buckets = sum(1 for line in open(hw))
+
+    prog = ["/home/hvatum/Skole/thesis/pest/pest", "/home/hvatum/Skole/thesis/workloads/m5out/" + test + "/trace.out", "-b", str(buckets), "-f", "plain", "-o", tf, "-w", weightfile]
+    if not os.path.isfile(tf):
+        print(" ".join(prog)+"\n")
+        os.system(" ".join(prog))
+    return align(read_graph(tf), read_graph(hw))
 
 
 
 def run_tests(output, weightfile):
-    prog = ["/home/hvatum/Skole/thesis/pest/pest", "/home/hvatum/Skole/thesis/workloads/m5out/pi-pi/trace.out", "-f", "plain", "-o", output, "-w", weightfile]
-    print(" ".join(prog))
-    subprocess.call(prog)
-#    process = subprocess.Popen(prog, shell=True, stdout=subprocess.PIPE)
-#    process.wait()
-#    print(process.returncode)
-    return align(read_graph(output), read_graph("/tmp/pi-power"))
+    return run_test(output, weightfile, "pi-pi") + run_test(output, weightfile, "trend-trend")
 
 #print(eval_individual([1,2,3,4,5,6,7,8,9]))
 #print(eval_individual([22,30,15,40,30,3,3,30,90]))
--- ga/ga.py
+++ ga/ga.py
@@ -6,7 +6,7 @@ from deap import creator, base, tools, algorithms
 NUM_WEIGHTS = 9
 toolbox = base.Toolbox()
 
-def create_population():
+def create_population(popsize):
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", array.array, typecode='H', fitness=creator.FitnessMin)
 
@@ -16,28 +16,75 @@ def create_population():
 
     toolbox.register("evaluate", eval_individual)
     toolbox.register("mate", tools.cxTwoPoints)
-    toolbox.register("mutate", tools.mutUniformInt, low=0, up=150, indpb=0.05)
-    toolbox.register("select", tools.selTournament, tournsize=5)
+    toolbox.register("mutate", tools.mutUniformInt, low=0, up=150, indpb=0.10)
+    toolbox.register("select", tools.selBest, k=1)
 
-    population = toolbox.population(n=3)
+    population = toolbox.population(n=popsize)
     return population
 
-def run_evolution(population, NGEN):
-    for gen in range(NGEN):
-        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
-        fits = toolbox.map(toolbox.evaluate, offspring)
-        for fit, ind in zip(fits, offspring):
+
+
+def run_evolution(population, ngen, file=None):
+    cxpb, mutpb = 0.0, 1
+#    stats = tools.Statistics()
+#    halloffame = tools.HallOfFame(20)
+#    final = algorithms.eaMuPlusLambda(pop, toolbox, 1, 4, CXPB, MUTPB, NGEN, stats, halloffame, True)
+#    print(halloffame)
+#    print(final)
+#    return final
+
+    if not file is None:
+        file.write("\nGeneration 0\n")
+
+    fitnesses = toolbox.map(toolbox.evaluate, population)
+    for ind, fit in zip(population, fitnesses):
+        ind.fitness.values = fit
+        if not file is None:
+            file.write(str(ind) + ", fitness: " + str(fit) + "\n")
+            file.flush()
+
+    for i in range(ngen):
+        file.write("\nGeneration " + str(i+1) + "\n")
+        offspring = algorithms.varOr(population, toolbox, 4, cxpb, mutpb)
+
+        fitnesses = toolbox.map(toolbox.evaluate, offspring)
+        for ind, fit in zip(offspring, fitnesses):
             ind.fitness.values = fit
-        population = offspring
-        sorted_pop = sorted(population, key=lambda ind: ind.fitness, reverse=False)
-        print(sorted_pop)
+            if not file is None:
+                file.write(str(ind) + ", fitness: " + str(fit) + "\n")
+                file.flush()
+        population = toolbox.select(population + offspring, 1)
     return population
+
+#    toolbox.evaluate(pop)
+#    for gen in range(NGEN):
+#        if not file is None:
+#            file.write("\nGeneration " + str(gen) + "\n")
+#        # Select and clone the next generation individuals
+#        offspring = map(toolbox.clone, toolbox.select(pop, len(pop)))
+#
+#        # Apply crossover and mutation on the offspring
+#        offspring = algorithms.varAnd(offspring, toolbox, CXPB, MUTPB)
+#
+#        # Evaluate the individuals with an invalid fitness
+#        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+#        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+#        for ind, fit in zip(invalid_ind, fitnesses):
+#            ind.fitness.values = fit
+#            if not file is None:
+#                file.write(str(ind) + ", fitness: " + str(fit) + "\n")
+#                file.flush()
+#
+#        # The population is entirely replaced by the offspring
+#        pop[:] = offspring
+#    return pop
 
 def main():
-    population = create_population()
-    #ind1 = toolbox.individual()
-    #print ind1
-    #print toolbox.mutate(ind1)[0]
-    print(run_evolution(population, 3))
+    popsize = 5
+    population = create_population(popsize)
+#    ind1 = toolbox.individual()
+#    ind1.fitness.values = toolbox.evaluate(ind1)
+#    print(str(ind1) + ", fitness: " + str(ind1.fitness.values))
+    run_evolution(population, 30, open("/home/hvatum/ga-results", "a"))
 
 main()
